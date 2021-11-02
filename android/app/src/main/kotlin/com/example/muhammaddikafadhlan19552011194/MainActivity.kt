6acf417efd21260cdda12ceac0e9f68358dafd16
unknown <fadlandika6@gmail.com>
2021-11-02 19:53:00 +0700

    Tugas

---
+++ android/app/src/main/kotlin/com/example/muhammaddikafadhlan19552011194/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.muhammaddikafadhlan19552011194
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:muhammaddikafadhlan19552011194/pages/main_page.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+
+}
